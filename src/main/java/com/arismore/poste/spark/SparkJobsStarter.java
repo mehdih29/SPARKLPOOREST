8724743d42cb15aed18727d4e285ab319f3a2ede
mehdih29 <mehdih29@gmail.com>
2015-10-06 17:02:50 +0200

    new versions

--- src/main/java/com/arismore/poste/spark/SparkJobsStarter.java
+++ src/main/java/com/arismore/poste/spark/SparkJobsStarter.java
@@ -1,44 +1,16 @@
 package main.java.com.arismore.poste.spark;
 
 
-import java.util.Properties;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-
-import main.java.com.arismore.poste.util.UniversalNamespaceCache;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.log4j.Logger;
-import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-
 import kafka.javaapi.producer.Producer;
-import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
 import org.apache.spark.SparkConf;
-import org.apache.spark.streaming.api.java.*;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+import javax.xml.parsers.DocumentBuilder;
+import java.util.List;
+import java.util.Properties;
 
 /**
  * Created by mehdi on 9/29/15.
@@ -48,7 +20,7 @@ public class SparkJobsStarter {
 
     private static final long serialVersionUID = 2222111111L;
 
-    static Logger LOG = Logger.getLogger(SparkJobsStarter.class);
+    //static Logger LOG = Logger.getLogger(SparkJobsStarter.class);
     private static String FILE_RECOVERY_WINDOWS = "/spark/POC/_file_recovery_window";
     private static DocumentBuilder builder = null;
 
@@ -66,10 +38,6 @@ public class SparkJobsStarter {
             System.exit(1);
         }
         try {
-            SparkConf sparkConf = new SparkConf().setAppName("JobStarter");
-            JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(60));
-            JavaDStream<String> receiverStream = jssc.textFileStream("file:///spark/POC/2");
-
             String STREAMING_API_URL = "http://national.cpn.prd.sie.courrier.intra.laposte.fr/National/enveloppes/v1/externe?";
             String SEP = "&";
             String BEGINDATE = "dateDebut=";
@@ -78,6 +46,9 @@ public class SparkJobsStarter {
             String COUNT = "count=";
             int STEP = 1000;
 
+            SparkConf sparkConf = new SparkConf().setAppName("JobStarter");
+            JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(60));
+            JavaDStream<List<String>> receiverStream = jssc.receiverStream(new HTTPCustomReceiver());
 
             props.put("metadata.broker.list", args[0]);
             props.put("serializer.class", "kafka.serializer.StringEncoder");
@@ -86,114 +57,39 @@ public class SparkJobsStarter {
             ProducerConfig config = new ProducerConfig(props);
             producer = new Producer<String, String>(config);
 
-            XPath xpath = XPathFactory.newInstance().newXPath();
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            factory.setNamespaceAware(true);
 
-            builder = factory.newDocumentBuilder();
+                if (receiverStream != null) {
+                    receiverStream.persist();
+                    receiverStream.print();
 
-
-            CloseableHttpClient client = HttpClientBuilder.create().build();
-            Date date = new Date();
-
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(date);
-            cal.add(Calendar.MINUTE, -6);
-            Date start = cal.getTime();
-
-            cal.setTime(date);
-            cal.add(Calendar.MINUTE, -5);
-            Date end = cal.getTime();
-
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm':00Z'");
-            formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
-
-            String dateDebut = formatter.format(start);
-            String dateFin = formatter.format(end);
-
-            LOG.debug("processing " + dateDebut + "  " + dateFin);
-
-            HttpGet get = new HttpGet(STREAMING_API_URL + BEGINDATE + dateDebut
-                    + SEP + ENDDATE + dateFin + SEP + STARTINDEX + "1" + SEP
-                    + COUNT + "1");
-            HttpResponse response;
-
-        try {
-            String url = null;
-
-            response = client.execute(get);
-            StatusLine status = response.getStatusLine();
-
-            if (status.getStatusCode() == 200) {
-
-                InputStream inputStream = response.getEntity().getContent();
-
-                Document doc = builder.parse(new InputSource(inputStream));
-                xpath.setNamespaceContext(new UniversalNamespaceCache(doc, true));
-
-                int number = Integer.parseInt((String) xpath.compile(
-                        "/a:feed/openSearch:totalResults").evaluate(doc,
-                        XPathConstants.STRING));
-                for (int i = 1; i < number; i += STEP) {
-                    String key = "";
-                    String msg = STREAMING_API_URL + BEGINDATE + dateDebut + SEP
-                            + ENDDATE + dateFin + SEP + STARTINDEX + i + SEP
-                            + COUNT + STEP;
-                    KeyedMessage<String, String> data = new KeyedMessage<String, String>(topic, key, msg);
-                    producer.send(data);
+                    /*for (int i = 1; i < number; i += STEP) {
+                        String key = "";
+                        String msg = STREAMING_API_URL + BEGINDATE + dateDebut + SEP
+                                + ENDDATE + dateFin + SEP + STARTINDEX + i + SEP
+                                + COUNT + STEP;
+                        KeyedMessage<String, String> data = new KeyedMessage<String, String>(topic, key, msg);
+                        producer.send(data);
+                    }*/
+                } else {
+                    System.out.println(receiverStream.toString());
+                    /*try {
+                        PrintWriter out = new PrintWriter(new FileWriter(
+                                FILE_RECOVERY_WINDOWS, true));
+                        out.println(STREAMING_API_URL + BEGINDATE + dateDebut + SEP
+                                + ENDDATE + dateFin + SEP + STARTINDEX + "1" + SEP
+                                + COUNT + "1");
+                        out.close();
+                        String key = "";
+                        String msg = STREAMING_API_URL + BEGINDATE + dateDebut + SEP
+                                + ENDDATE + dateFin + SEP + STARTINDEX + "1" + SEP
+                                + COUNT + "1";
+                        KeyedMessage<String, String> data = new KeyedMessage<String, String>(catchingTopic, key, msg);
+                        producer.send(data);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }*/
                 }
-            } else {
-                try {
-                    PrintWriter out = new PrintWriter(new FileWriter(
-                            FILE_RECOVERY_WINDOWS, true));
-                    out.println(STREAMING_API_URL + BEGINDATE + dateDebut + SEP
-                            + ENDDATE + dateFin + SEP + STARTINDEX + "1" + SEP
-                            + COUNT + "1");
-                    out.close();
-                    String key = "";
-                    String msg = STREAMING_API_URL + BEGINDATE + dateDebut + SEP
-                            + ENDDATE + dateFin + SEP + STARTINDEX + "1" + SEP
-                            + COUNT + "1";
-                    KeyedMessage<String, String> data = new KeyedMessage<String, String>(catchingTopic, key, msg);
-                    producer.send(data);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
 
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            LOG.error("Error in communication with the OREST TAE api ["
-                    + get.getURI().toString() + "]");
-            try {
-                PrintWriter out = new PrintWriter(new FileWriter(
-                        FILE_RECOVERY_WINDOWS, true));
-                out.println(STREAMING_API_URL + BEGINDATE + dateDebut + SEP
-                        + ENDDATE + dateFin + SEP + STARTINDEX + "1" + SEP
-                        + COUNT + "1");
-                out.close();
-                String key = "";
-                String msg = STREAMING_API_URL + BEGINDATE + dateDebut + SEP
-                        + ENDDATE + dateFin + SEP + STARTINDEX + "1" + SEP
-                        + COUNT + "1";
-                KeyedMessage<String, String> data = new KeyedMessage<String, String>(catchingTopic, key, msg);
-                producer.send(data);
-            } catch (IOException a) {
-                a.printStackTrace();
-            }
-        } catch (SAXException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (XPathExpressionException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-            receiverStream.print();
             jssc.start();
             jssc.awaitTermination();
         }catch(Exception e) {
